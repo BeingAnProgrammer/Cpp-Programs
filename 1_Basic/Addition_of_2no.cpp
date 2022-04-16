a4e00934f22727b0672edb5ddad7ea75f00f6119
Nitesh Kumar <nitesh20rv@gmail.com>
2022-04-16 19:04:28 +0530

    Addition of 2 no

---
+++ 1_Basic/Addition_of_2no.cpp
@@ -0,0 +1,15 @@
+// Addition of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the value of a : "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b : "<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<"The addition of two no is = "<<c;
+    
+    return 0;
+}
