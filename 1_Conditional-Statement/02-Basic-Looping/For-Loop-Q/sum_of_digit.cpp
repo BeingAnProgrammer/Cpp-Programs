e574bee90b46e51fae5b5d6fcdfca38e58987d1e
Nitesh Kumar <nitesh20rv@gmail.com>
2022-04-18 18:16:34 +0530

    Finding sum of digits of a no

---
+++ 1_Conditional-Statement/02-Basic-Looping/For-Loop-Q/sum_of_digit.cpp
@@ -0,0 +1,22 @@
+/* This is a program to find the sum of digits of a given no using for loop 
+
+  meaning    lets say user give a no n=237 
+               then the output will be        2 + 3 + 7 = 12      */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum=0,dig;
+    cout<<"Enter a no to find the sum of digits = ";
+    cin>>n;
+    dig=n;   //stored the value of n in dig so that i can use it to display at the no at the end
+
+    for (; n>0; n=n/10)
+    {
+        sum=sum+(n%10);
+    }
+    
+    cout<<"sum of digits of no "<<dig<<" = "<<sum;
+    return 0;
+}
