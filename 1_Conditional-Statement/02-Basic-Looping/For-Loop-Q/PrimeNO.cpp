16e01127e6dc6401a88b4202b46b330b71e7d184
Nitesh Kumar <nitesh20rv@gmail.com>
2022-04-21 17:32:22 +0530

    Prime number

---
+++ 1_Conditional-Statement/02-Basic-Looping/For-Loop-Q/PrimeNO.cpp
@@ -0,0 +1,23 @@
+// This is a program to check whether a no is prime or not using for loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, j, count = 0;
+    cout << "Enter a no to check if its Prime no = ";
+    cin >> n;
+
+    for (j = 1; j <= n; j++)
+    {
+        if (n % j == 0)
+            count = count + 1;
+    }
+
+    if (count == 2)
+        cout << n << " is a prime no ";
+    else
+        cout << n << " is not a prime no!!";
+
+    return 0;
+}
---
+++ 1_Conditional-Statement/02-Basic-Looping/While-Loop-Q/PrimeNo_.cpp
@@ -0,0 +1,25 @@
+// This is a program to check whether a no is prime or not using while loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, j, count = 0;
+    cout << "Enter a no to check if its Prime no = ";
+    cin >> n;
+
+    j = 1;
+    while (j <= n)
+    {
+        if (n % j == 0)
+            count = count + 1;
+
+        j++;
+    }
+    if (count == 2)
+        cout << n << " is a prime no ";
+    else
+        cout << n << " is not a prime no!!";
+
+    return 0;
+}
