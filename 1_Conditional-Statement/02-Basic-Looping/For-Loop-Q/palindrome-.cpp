5648d9c6144e84473953a298770c8a40b6862eaf
Nitesh Kumar <nitesh20rv@gmail.com>
2022-04-21 16:01:47 +0530

    Palindrome

---
+++ 1_Conditional-Statement/02-Basic-Looping/For-Loop-Q/palindrome-.cpp
@@ -0,0 +1,29 @@
+/* This is a program to check whether a no is palindrome or not using for loop
+  What is palindrome:
+  A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward
+
+  for eg if the user inputs n=232  , since its reverse is also same so its a palindrome
+    or n=123 its rev is 321 which is not same as the original so its not palindrome             */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n, rev = 0, save;
+    cout << "Enter a no to find if its palinderome or not = ";
+    cin >> n;
+    save = n;
+
+    for (; n > 0; n = n / 10)
+    {
+        rev = rev * 10 + n % 10;
+    }
+
+    if (rev == save)
+        cout << "Its palindrome!!";
+    else
+        cout << "Its Not a palindrome!!";
+
+    return 0;
+}
