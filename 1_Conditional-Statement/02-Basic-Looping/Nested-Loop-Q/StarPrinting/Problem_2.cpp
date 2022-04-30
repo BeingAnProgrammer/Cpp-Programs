08e572d57c53fbaeecb72f25654993c9287b4e09
Nitesh Kumar <nitesh20rv@gmail.com>
2022-04-30 20:55:43 +0530

    Problem_2

---
+++ 1_Conditional-Statement/02-Basic-Looping/Nested-Loop-Q/StarPrinting/Problem_2.cpp
@@ -0,0 +1,25 @@
+/* Program to print
+1
+12
+123
+1234
+12345
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
