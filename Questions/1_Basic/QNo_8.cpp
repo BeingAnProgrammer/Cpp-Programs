43e2fa922c745371355c3283a68e13d52c3f47da
Nitesh Kumar <nitesh20rv@gmail.com>
2022-05-02 20:58:40 +0530

    Program to Multiply two decimal Numbers entered by User

---
+++ Questions/1_Basic/QNo_8.cpp
@@ -0,0 +1,18 @@
+// ) Write a Program to Multiply two decimal Numbers entered by User
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cout << "Enter 1st number = ";
+    cin >> a;
+    cout << "Enter 2nd number = ";
+    cin >> b;
+
+    float c;
+    c = a * b;
+    cout << "Multiplication of the two no are = " << c;
+
+    return 0;
+}
