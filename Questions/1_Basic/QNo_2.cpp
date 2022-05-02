71bcfd69c615bdd7cd1486fea8ffe97c6243e47e
Nitesh Kumar <nitesh20rv@gmail.com>
2022-05-02 19:42:47 +0530

    Program to Print Integer Number Entered by User

---
+++ Questions/1_Basic/QNo_2.cpp
@@ -0,0 +1,14 @@
+//  Write a Program to Print Integer Number Entered by User
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter a number = ";
+    cin >> a;
+
+    cout << a;
+
+    return 0;
+}
