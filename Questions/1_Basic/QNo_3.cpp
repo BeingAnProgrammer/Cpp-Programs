ac10988b69989fd28acd2c5eb7f06962f8a8dff2
Nitesh Kumar <nitesh20rv@gmail.com>
2022-05-02 19:49:32 +0530

    Add Two Integer Numbers Entered by User

---
+++ Questions/1_Basic/QNo_3.cpp
@@ -0,0 +1,16 @@
+// Write a Program to Add Two Integer Numbers Entered by User
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "\nEnter first numbers = ";
+    cin >> a;
+    cout << "\nEnter second numbers = ";
+    cin >> b;
+
+    cout << "Sum = " << a + b;
+
+    return 0;
+}
