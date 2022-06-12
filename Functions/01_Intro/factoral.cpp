58f000d70531bc8991c911133906bcadf291084d
Nitesh Kumar <nitesh20rv@gmail.com>
2022-06-12 09:16:27 +0530

    factorial using function

--- Functions/01_Intro/factoral.cpp
+++ Functions/01_Intro/factoral.cpp
@@ -1,25 +1,29 @@
-// To find the factorial of a number using function 
+// To find the factorial of a number using function
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-
 
 int factoral(int n)
 {
-    if(n<=1)
-    return 1;
+    if (n <= 1)
+        return 1;
 
-    return n * factoral(n-1);
+    return n * factoral(n - 1);
 }
+// Step by step calculation of factorial
+// factorial(4)=4*factorial(3);
+// factorial(4)=4*3*factorial(2);
+// factorial(4)=4*3*2*factorial(1);
+// factorial(4)=4*3*2*1;
+// factorial(4)=24;
 int main()
 {
 
     int a;
-    cout<<"Enter a no to find its factoral = ";
-    cin>>a;
+    cout << "Enter a no to find its factoral = ";
+    cin >> a;
 
-    cout<<"The factorila of "<<a<<" is = "<<factoral(a);
+    cout << "The factorila of " << a << " is = " << factoral(a);
 
-return 0;
+    return 0;
 }
